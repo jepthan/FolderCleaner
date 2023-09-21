3c470362064e5c63fde76f88c6705aeb1ab7a00b
jepthan <50557700+jepthan@users.noreply.github.com>
2023-09-20 22:21:29 -0600

    fix bugs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import re
 import json
+import sys
 
 
 def FolderSetup(clenerpath, ListOfDest):
@@ -19,13 +20,17 @@ def extencionToString(extencions: list) -> str:
     return exten
 
 
-if __name__ == '__main__':
-    path = "C://Users//jefte//Downloads"
-    clenerpath = "C://Users//jefte//Documents//File_Clener"
-    files = os.listdir(path)
+def loadJson(clenerpath) ->list:
+    f = open("FileExtencionData.json", "r")
+    fr = f.read()
+    print("LOADING JSON FILE" )
+    ListOfDest = json.loads(fr)
+    return ListOfDest
+def CreateDefaultJSON(clenerpath) -> list:
+    print("CREATING DEFAULT JSON FILE")
     ListOfDest = [
         {"path": clenerpath + "//Documents", "ext": ["docx", "doc", "pdf", "xlsx", "ppt", "txt"]},
-        {"path": clenerpath + "//Images", "ext": ["jpg", "svg", "gif", "png", "webm"]},
+        {"path": clenerpath + "//Images", "ext": ["jpg","jpeg", "svg", "gif", "png", "webm"]},
         {"path": clenerpath + "//folders", "ext": []},
         {"path": clenerpath + "//compressed files", "ext": ["zip", "rar", "gz"]},
         {"path": clenerpath + "//installers", "ext": ["msi", "tar", "jar"]},
@@ -37,17 +42,35 @@ if __name__ == '__main__':
 
     f = open("FileExtencionData.json", "w")
     f.write(json.dumps(ListOfDest))
-    print(json.dumps(ListOfDest))
+    return ListOfDest
+
+def main():
+    path = "C://Users//jefte//Downloads"
+    clenerpath = "C://Users//jefte//Documents//File_Clener"
+    files = os.listdir(path)
+    ListOfDest = loadJson(clenerpath)
+    if not os.path.isfile("FileExtencionData.json"):
+        ListOfDest = CreateDefaultJSON(clenerpath)
+
+    for item in ListOfDest:
+        print(item["path"] + "       " + extencionToString(item["ext"]))
+    comfirm = input("Are you sure you want to move the files (Y/N)")
+
+    if (comfirm != "Y"):
+        print("Bye then")
+        return 0
 
     # FolderSetup(clenerpath, ListOfDest)
 
-    # for dest in ListOfDest:
-    #     for file in files:
-    #         if re.search("^.*\.("+extencionToString(dest.extencions)+")$", file):
-    #             print(dest.path +"----------------"+ file)
-    #             print(path +"//"+ file)
-    #             shutil.move(path+"//"+file, dest.path)
-    #         if (extencionToString(dest.extencions) == ""):
-    #             if os.path.isdir(path+"//"+file):
-    #                 print(file)
-    #                 shutil.move(path+"//"+file, dest.path)
+    for dest in ListOfDest:
+        for file in files:
+            if re.search("^.*\.("+extencionToString(dest["ext"])+")$", file):
+                print("archivo:"+ file+" Movido a")
+                shutil.move(path+"//"+file, dest["path"])
+            if (extencionToString(dest["ext"]) == ""):
+                if os.path.isdir(path+"//"+file):
+                    print(file)
+                    shutil.move(path+"//"+file, dest["path"])
+
+if __name__ == '__main__':
+    main()
